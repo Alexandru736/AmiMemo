59091f4a966821b1ad54c7f62d67ca4b3f68ebc0
lidiamadaras <madaras.lidia@student.ms.sapientia.ro>
2023-07-14 15:35:04 +0300

    First question page commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,207 @@
 import 'package:flutter/material.dart';
 
+import 'flashcard.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: StartPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class StartPage extends StatelessWidget {
+  StartPage({Key? key}) : super(key: key);
+  DateTime date = DateTime.now();
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome to the Practice App!'),
+      ),
+      body: Center(
+        child: Text(
+          'Press the button to start practicing!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => PracticePage(
+                flashcard: Flashcard(
+                  "123",
+                  "Inima",
+                  "Un organ",
+                  "Inima este un organ muscular care pompează sângele prin corp",
+                  date,
+                  "organ",
+                  "jucarie",
+                  "obiect",
+                ),
+              ),
+            ),
+          );
+        },
+        child: Icon(Icons.play_arrow),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class PracticePage extends StatefulWidget {
+  final Flashcard flashcard;
 
-  void _incrementCounter() {
+  PracticePage({Key? key, required this.flashcard}) : super(key: key);
+
+  @override
+  _PracticePageState createState() => _PracticePageState();
+}
+
+class _PracticePageState extends State<PracticePage> {
+  String selectedWord = '';
+  List<String> options = [];
+  String content = '';
+
+
+  @override
+  void initState() {
+    super.initState();
+    options.add(widget.flashcard.correctWord);
+    options.add(widget.flashcard.wrongWord1);
+    options.add(widget.flashcard.wrongWord2);
+    options.shuffle();
+    content = widget.flashcard.content.replaceAll(widget.flashcard.correctWord, '___');
+  }
+
+  void wordReplace(String buttonText) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      selectedWord = buttonText;
+      content = content.replaceFirst('___', selectedWord);
     });
+  }
+
+  void checkAnswer() {
+    if (selectedWord == '') {
+      return;
+    }
+    String message;
+    if (selectedWord == widget.flashcard.correctWord) {
+      message = 'Well done!';
+    } else {
+      message = 'Wrong answer. The correct answer is: ${widget.flashcard.correctWord}.';
+    }
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Result'),
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                setState(() {
+                  selectedWord = '';
+                  content = widget.flashcard.content.replaceFirst('___', '___');
+                  options.shuffle(); // shuffle options again
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => StartPage(), // navigate back to StartPage
+                    ),
+                  );
+                });
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Practice!'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            SizedBox(height: 30),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              content,
+              style: TextStyle(fontSize: 30, color: Colors.deepPurple),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: selectedWord == '' ? () => wordReplace(options[0]) : null,
+                  child:Text(options[0]),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: selectedWord == '' ? () => wordReplace(options[1]) : null,
+                  child: Text(options[1]),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: selectedWord == '' ? () => wordReplace(options[2]) : null,
+                  child: Text(options[2]),
+                ),
+              ],
+            ),
+            SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () => checkAnswer(),
+              child: Text('Check Answer'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+class NextWord extends StatelessWidget {
+  const NextWord({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('Next Word'),
+      ),
+      body: Center(
+        child: Text(
+          'Let\'s move on to the next word.',
+          style: TextStyle(fontSize: 24),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
+
