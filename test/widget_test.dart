719113ca5a92a8c19806b53ae21eec553836c83d
Alexandru736 <ionitaalexandru703@yahoo.com>
2023-07-16 02:49:55 +0300

    Renamed homepage.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mobile_catenav4/main.dart';
+import 'package:mobile_catenav4/homepage.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
